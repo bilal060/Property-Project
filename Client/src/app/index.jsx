b6a00a41396a813ac758b34d843051c9c6bc089d
Muhammad Bilal <ahmad_4al@hotmail.com>
2022-09-09 12:23:40 +0500

    pkg install

--- Client/src/app/index.jsx
+++ Client/src/app/index.jsx
@@ -24,6 +24,7 @@ function App() {
   return (
     <>
       <Layout>
+        
         <h1>Hellow World</h1>
       </Layout>
     </>
--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 import './styles/app.css';
 
@@ -10,7 +10,9 @@ import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
+   <BrowserRouter>
     <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
