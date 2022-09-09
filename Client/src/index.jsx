e0f40e53952a08e3d06d13f135057aba22dcafc9
Muhammad Bilal <ahmad_4al@hotmail.com>
2022-09-09 12:26:39 +0500

    f

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -4,10 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 import './styles/app.css';
 
-import { Router as RouterHistory } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <Provider store={store}>
    <BrowserRouter>
